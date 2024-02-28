9d9610bc879d0ec19a2ab82fca70a1e39c35b570
Mateo <mateo.palomino21@hotmail.com>
2024-02-27 19:05:14 -0500

    update small details that just pop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "MTL Preserves",
   description: "Local MTL Company",
+  icon: "",
 };
 
 export default function RootLayout({
